0f054345f692d65e0d255c59877822ba695de08a
slaveoftime <albertwoo_slaveoftime@hotmail.com>
2021-01-12 17:03:38 +0800

    Update packages

--- src/Client/www/postcss.config.js
+++ src/Client/www/postcss.config.js
@@ -19,12 +19,11 @@ module.exports = {
     plugins: [
         require('tailwindcss'),
         require('autoprefixer'),
-        require('postcss-nested'),
-        // ...process.env.NODE_ENV === 'production'
-        //     ? [
-        //         purgecss,
-        //         require('cssnano')
-        //     ]
-        //     : []
+        ...process.env.NODE_ENV === 'production'
+            ? [
+                purgecss,
+                require('cssnano')
+            ]
+            : []
     ]
 }
